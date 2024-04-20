833220162e60ed4308b8fd608777118950fbefd2
Wendy <82058944+WendyWang29@users.noreply.github.com>
2024-04-20 17:40:15 +0200

    plot in s

--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@ from src.resnet_utils import get_features
 from src.utils import *
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
+from matplotlib.ticker import FuncFormatter
 
 def load_spec_model(device, config):
     """
@@ -16,10 +18,21 @@ def load_spec_model(device, config):
     return resnet_spec_model
 
 def plot_spec(spec, index):
+    hop_length = 512
+    sr = 16000
+    frame_duration = hop_length/sr
+
+    # compute time value for each frame
+    frame_times = np.arange(spec.shape[1]) * frame_duration
+
     plt.figure(figsize=(10, 6))
     plt.imshow(spec, aspect='auto', origin='lower', cmap='viridis')
-    plt.xlabel('Time frame')
+    plt.xlabel('Time (s)')
+
+    # Set the tick positions and labels based on frame times
+    plt.xticks(np.arange(0, spec.shape[1], 5), frame_times[::5])
     plt.ylabel('Frequency bin')
+    plt.colorbar(label='Intensity (dB)')
     plt.title(f'Magnitude spectrogram for file {index} ')
     plt.show()
 
@@ -49,7 +62,6 @@ def eval_one_file_spec(index, config):
 
     # plot the spectrogram
     plot_spec(X, index)
-    #TODO check if time is correct
 
     # transform into a mini batch and to a tensor
     X_batch = np.expand_dims(X, axis=0)  # shape ndarray (1,1025,41)
@@ -62,8 +74,12 @@ def eval_one_file_spec(index, config):
 
 
 if __name__ == '__main__':
+    matplotlib_logger = logging.getLogger('matplotlib.font_manager')
+    matplotlib_logger.setLevel(logging.ERROR)
+
     seed_everything(1234)
     set_gpu(-1)
+
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
